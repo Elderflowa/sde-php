2cfbf7539367fcd14e6e7d486857928dc3a44b18
Nilas <Nilas@DESKTOP-CGM04I6.home>
2024-03-17 21:35:29 +0100

    home commit 2

---
+++ ProjectQuiz/template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Regler</title>
+    </head>
+    <body>
+        <?php
+        $sp1 = "Hvad er englands hovedstad?";
+        
+        $sp1a = "London";
+        $sp1b = "Landan";
+        $sp1c = "Lindin";
+        $sp1d = "Lundun";
+        
+        $sp1r = "a"
+        ?>
+    </body>
+</html>
